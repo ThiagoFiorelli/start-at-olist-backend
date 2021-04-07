3ebecc234a0ccca2b555b6e9c45deadd13ea78cc
Thiago Fiorelli <thiago.fiorelli@olist.com>
2021-04-07 17:56:58 -0300

    Add Bill serializer.

--- api/apps/calls/serializers.py
+++ api/apps/calls/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
-from .models import CallRecord
+from .models import CallRecord, Bill
 
 
 class CallRecordSerializer(serializers.ModelSerializer):
     class Meta:
         model = CallRecord
-        fields = '__all__'
+        fields = "__all__"
+
+
+class BillSerializer(serializers.ModelSerializer):
+    call_records = CallRecordSerializer(many=True)
+
+    class Meta:
+        model = Bill
+        fields = ["source", "period", "call_records", "total_price"]
